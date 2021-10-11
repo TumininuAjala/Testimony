c2fb4f36968c6731dcefe4fc11f34d059fda1c25
TumininuAjala <92306403+TumininuAjala@users.noreply.github.com>
2021-10-11 14:33:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
 
     for (let i = 0; i < 10; i++) {
         slide1.animate({ left: '30%' }, 5000);
-        slide2.animate({ left: '-50%' }, 5000);
-        slide3.animate({ left: '150%' }, 0);
+        slide1.animate({ left: '-50%' }, 5000);
+        slide1.animate({ left: '150%' }, 0);
 
         // slide2.animate({ left: '30%' }, 5000);
         // slide2.animate({ left: '-50%' }, 5000);
