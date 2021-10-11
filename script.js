6d3f025ca0475c1a7a30b83fe432e23c4428d44b
Ajala Tumininu <ajalatumininu@gmail.com>
2021-10-11 14:26:32 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    let slide1 = $("#slide1");
+    let slide2 = $("#slide2");
+    let slide3 = $("#slide3");
+
+    for (let i = 0; i < 10; i++) {
+        slide1.animate({ left: '30%' }, 5000);
+        slide2.animate({ left: '-50%' }, 5000);
+        slide3.animate({ left: '150%' }, 0);
+
+        // slide2.animate({ left: '30%' }, 5000);
+        // slide2.animate({ left: '-50%' }, 5000);
+        // slide2.animate({ left: '150%' }, 0);
+
+        // slide3.animate({ left: '30%' }, 5000);
+        // slide3.animate({ left: '-50%' }, 5000);
+        // slide3.animate({ left: '150%' }, 0);
+
+    }
+});
